840f3fe472ebd743de045c01a093a57cef2f6859
John <jparkteach@yahoo.com>
2017-04-02 20:56:37 -0400

    NeoCambio Project

--- app/src/main/java/net/jptechnology/android/neocambio/MainActivity.java
+++ app/src/main/java/net/jptechnology/android/neocambio/MainActivity.java
@@ -1,7 +1,9 @@
 package net.jptechnology.android.neocambio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void button(View view) {
+        Intent i = new Intent(this, SignUp.class);
+        startActivity(i);
     }
 }
